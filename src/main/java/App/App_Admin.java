6473678f12af7441d5434f6b5fab5db22e478d05
Hoangduy2982003 <125873702+Hoangduy2982003@users.noreply.github.com>
2024-01-22 18:59:03 +0700

    lay code

--- src/main/java/App/App_Admin.java
+++ src/main/java/App/App_Admin.java
@@ -1,12 +1,20 @@
 package App;
 
 import java.awt.EventQueue;
+import java.awt.Graphics2D;
+import java.awt.Image;
+import java.awt.image.BufferedImage;
+import java.io.File;
+import java.io.IOException;
 
 import javax.swing.JFrame;
 import javax.swing.JPanel;
 import javax.swing.border.EmptyBorder;
 import java.awt.Color;
 import javax.swing.JDesktopPane;
+import javax.swing.JLabel;
+import javax.imageio.ImageIO;
+import javax.swing.ImageIcon;
 
 public class App_Admin extends JFrame {
 
@@ -48,11 +56,13 @@ public class App_Admin extends JFrame {
 		panelLateral.setBackground(Color.WHITE);
 		panelLateral.setBounds(0,0,219,663);
 		contentPane.add(panelLateral);
+		panelLateral.setLayout(null);
 		
 		panelTop = new JPanel();
 		panelTop.setBackground(new Color(102, 0, 255));
 		panelTop.setBounds(223, 0, 957, 37);
 		contentPane.add(panelTop);
+		panelTop.setLayout(null);
 		
 		desktopPane = new JDesktopPane();
 		desktopPane.setBounds(223, 37, 957, 626);
